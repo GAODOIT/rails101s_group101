f6c9f027c2d7e06abd205189fb63f1f79bf49a07
CentOs_work <905809284@qq.com>
2015-06-26 08:04:46 +0800

    add more file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+def notice_message
+    alert_types = { :notice => :success, :alert => :danger }
+    
+    close_button_options = { :class => "close", "data-dismiss" => "alert", "aria-hidden" => true }
+    close_button = content_tag(:button, "×", close_button_options)
+    puts close_button #<button class="close" data-dismiss="alert" aria-hidden="true">×</button>
+
+    alerts = flash.map do |type, message|
+        alert_content = close_button + message
+        alert_type = alert_types[type.to_sym] || type
+        alert_class = "alert alert-#{alert_type} alert-dismissable"
+        content_tag(:div, alert_content, :class => alert_class)
+    end
+
+    alerts.join("\n").html_safe
+end
+
 end
