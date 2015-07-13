1ef2075f7861b6e323c47e5fa395a7fcdc4ed591
CentOs_work <905809284@qq.com>
2015-07-13 23:48:17 +0800

    优化了posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
+    before_action :find_group, :only => [:new, :edit, :create, :update, :destroy]
 
     def new
-        @group = Group.find(params[:group_id])
+        #@group = Group.find(params[:group_id])
         @post  = @group.posts.new
     end
 
     def edit
-        @group = Group.find(params[:group_id])
+        #@group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
     end
 
     def create
-        @group = Group.find(params[:group_id])
+        #@group = Group.find(params[:group_id])
         @post = @group.posts.new(post_params)
         if @post.save
             redirect_to group_path(@group), :notice => '新增文章成功！ '
@@ -21,7 +22,7 @@ class PostsController < ApplicationController
     end
 
     def update
-        @group = Group.find(params[:group_id])
+        #@group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
         if @post.update(post_params)
             redirect_to group_path(@group), :notice => '文章修改成功！ '
@@ -31,14 +32,20 @@ class PostsController < ApplicationController
     end
 
     def destroy 
-        @group = Group.find(params[:group_id])
+        #@group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
         @post.destroy
             redirect_to group_path(@group), :alert => '文章已刪除'
     end
 
+    private
+
     def post_params
         params.require(:post).permit(:content)
+    end
+
+    def find_group
+        @group = Group.find(params[:group_id])
     end
 
 end
